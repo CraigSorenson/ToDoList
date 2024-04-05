9b81d543f2fdf4b7ce226810d09112f908b1a319
Craig Sorenson <craig.sorensonx1@gmail.com>
2024-04-05 02:16:16 -0600

    Focus on input, Mark complete, export as JSON, code cleanup

--- README.md
+++ README.md
@@ -10,14 +10,19 @@ Craig Sorenson
 ### 20240404-222x Initial Commit
 Basic working todo list with ability to add and delete notes.
 
-## Todo
+### 20240405-0216
 * Return focus to input field
+* Stored creation date in milliseconds to facilitate sorting in future
+* Added ability to to mark complete.
+* Code cleanup
+* Export list as JSON
+
+## Todo
+* Import list as JSON
 * Add ability to set due date
-* Add ability to to mark complete.
 * Show hours/days since creation
-* Export/Import list as JSON
-* Export list as text
 * Fix CSS and make prettier
+* Export list as text
 
 ## License
 GNU GPLv3
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,10 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { FaPlus, FaTrashAlt } from "react-icons/fa";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  FaPlus,
+  FaTrashAlt,
+  FaCheck,
+  FaFileExport,
+  FaFileImport,
+} from "react-icons/fa";
+
+const makeFileDateTime = (ms) => {
+  const dateObj = new Date(ms);
+  let dateStamp = "";
+  dateStamp += dateObj.getFullYear().toString();
+  dateStamp += (dateObj.getMonth() + 1).toString().padStart(2, "0");
+  dateStamp += (dateObj.getDay() + 1).toString().padStart(2, "0");
+  dateStamp += "_";
+  dateStamp += dateObj.getHours().toString().padStart(2, "0");
+  dateStamp += dateObj.getMinutes().toString().padStart(2, "0");
+  dateStamp += dateObj.getSeconds().toString().padStart(2, "0");
+  return dateStamp;
+};
+
+const exportJSON = async (
+  toDoData,
+  filename = `${makeFileDateTime(Date.now())}-todolist.json`
+) => {
+  const data = {
+    version: "1.0.0",
+    timestamp: Date.now(),
+    data: toDoData,
+  };
+  const blob = new Blob([JSON.stringify(data, null, 2)], {
+    type: "application/json",
+  });
+  const a = document.createElement("a");
+  a.download = filename;
+  a.href = URL.createObjectURL(blob);
+  a.addEventListener("click", (e) => {
+    setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+  });
+  a.click();
+};
 
 const ToDoList = () => {
   const [todoInput, setTodoInput] = useState("");
   const [addItemEnabled, setAddITemEnabled] = useState(true);
   const [list, setList] = useState([]);
+
+  const inputRef = useRef();
 
   const handleListItemInput = (e) => {
     e.preventDefault();
@@ -12,19 +54,37 @@ const ToDoList = () => {
   };
 
   const handleAddListItem = () => {
-    const uuid = crypto.randomUUID();
-    const date = new Date(Date.now());
+    if (todoInput === "") return;
+    const timestamp = Date.now();
     const listItem = {
       note: todoInput,
-      uuid: uuid,
-      created: date.toString(),
+      uuid: crypto.randomUUID(),
+      created: timestamp,
+      complete: false,
+      dueDate: timestamp + 86400000,
     };
     setList([...list, listItem]);
     setTodoInput("");
+    inputRef.current.focus();
   };
 
   const handleDeleteItem = (id) => {
     setList(list.filter((item) => id !== item.uuid));
+  };
+
+  const handleMarkComplete = (id, complete) => {
+    setList(
+      list.map((item) => {
+        if (item.uuid === id) {
+          return { ...item, complete };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+  const handleFileExportJSON = () => {
+    exportJSON(list);
   };
 
   useEffect(() => {
@@ -36,34 +96,56 @@ const ToDoList = () => {
       <h1>Cygnus ToDo List</h1>
       <div className="addItem">
         <input
-            onKeyDown={(e) => {
-              if (e.key === "Enter")
-                  handleAddListItem();
-              }}
-          autoFocus
-          type="text"
-          name="todoItem"
           id="toDoITem"
+          name="todoItem"
+          type="text"
+          autoFocus
+          ref={inputRef}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleAddListItem();
+          }}
           onChange={handleListItemInput}
           value={todoInput}
         />
         <button onClick={handleAddListItem} disabled={addItemEnabled}>
           <FaPlus />
         </button>
+        <button onClick={() => handleFileExportJSON()} tooltip="Export">
+          <FaFileExport />
+        </button>
+        {/* <input
+          type="file"
+          className="hidden"
+          multiple={false}
+          accept=".json,application/json"
+          onChange={(evt) => this.openFile(evt)}
+        /> */}
+        {/* <button onClick={() => handleFileImportJSON()}>
+          <FaFileImport />
+        </button> */}
       </div>
       <ul className="itemList">
         {list.map((item) => {
-          new Date(item.created);
+          // new Date(item.created);
           return (
             <li key={item.uuid} className="listItem">
               <div>
-                <p>Created: {item.created}</p>
+                <p>Created: {new Date(item.created).toLocaleString()}</p>
+                <p>Due: {new Date(item.dueDate).toLocaleString()}</p>
                 <p>Note: {item.note}</p>
+                <p>Complete: {item.complete ? "Yes" : "No"}</p>
                 <p>ID: {item.uuid}</p>
               </div>
-              <button onClick={() => handleDeleteItem(item.uuid)}>
-                <FaTrashAlt />
-              </button>
+              <div>
+                <button onClick={() => handleDeleteItem(item.uuid)}>
+                  <FaTrashAlt />
+                </button>
+                <button
+                  onClick={() => handleMarkComplete(item.uuid, !item.complete)}
+                >
+                  <FaCheck />
+                </button>
+              </div>
             </li>
           );
         })}
